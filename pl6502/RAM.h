2fa165548ddd355c0e91093c92e9e463b6139bfb
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-10-25 20:15:27 -0400

    ok

--- pl6502/RAM.h
+++ pl6502/RAM.h
@@ -7,12 +7,11 @@
 
 class RAM : public component {
     public:
-        input<16> address ;
-        input<8> data_in ;
+        input<8> data_in, addrh, addrl ;
         input<1> set, enable ;
         output<8> data_out ;
     private:
-        uint8_t _mem[0x10000] ;
+        uint8_t _mem[256][256] ;
 
     public:
         RAM() : set(this), enable(this) {
@@ -21,11 +20,11 @@ class RAM : public component {
 
         void always(){
             if (set){
-                _mem[address] = data_in ;
+                _mem[addrh][addrl] = data_in ;
             }
             if (enable){
                 data_out.drive(true) ;
-                data_out = _mem[address] ;
+                data_out = _mem[addrh][addrl] ;
             }
             else {
                 data_out.drive(false) ;
