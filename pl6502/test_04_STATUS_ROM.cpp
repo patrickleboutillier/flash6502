0c5b81f2e63f30fe5cc967128e5f9ad4ce818fb7
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-11-10 08:48:12 -0500

    ok

--- pl6502/test_04_STATUS_ROM.cpp
+++ pl6502/test_04_STATUS_ROM.cpp
@@ -6,6 +6,7 @@
 
 TEST(test_pl6502, STATUS_ROM){
     output<1> n_in, v_in, z_in, c_in, b_in ;
+    output<1> n_old, v_old, z_old, c_old, alu_c_old ;
     output<1> nz_set, v_set, c_set, alu_c_set, alu_c_from_C ;
     STATUS_ROM status_rom ;
     n_in.connect(status_rom.n_in) ;
@@ -18,20 +19,18 @@ TEST(test_pl6502, STATUS_ROM){
     c_set.connect(status_rom.c_set) ;
     alu_c_set.connect(status_rom.alu_c_set) ;
     alu_c_from_C.connect(status_rom.alu_c_from_C) ;
+    n_old.connect(status_rom.n_old) ;
+    v_old.connect(status_rom.v_old) ;
+    z_old.connect(status_rom.z_old) ;
+    c_old.connect(status_rom.c_old) ;
+    alu_c_old.connect(status_rom.alu_c_old) ;
 
-    status_rom.N.connect(status_rom.n_old) ;
-    status_rom.V.connect(status_rom.v_old) ;
-    status_rom.Z.connect(status_rom.z_old) ;
-    status_rom.C.connect(status_rom.c_old) ;
-    status_rom.alu_c.connect(status_rom.alu_c_old) ;
-
-
-    for (int i = 0 ; i < 1024 ; i++){
-        uint32_t n_old = status_rom.N ;
-        uint32_t v_old = status_rom.V ;
-        uint32_t z_old = status_rom.Z ;
-        uint32_t c_old = status_rom.C ;
-        uint32_t alu_c_old = status_rom.alu_c ;
+    for (int i = 0 ; i < 32768 ; i++){
+        n_old = i >> 14 ;
+        v_old = i >> 13 ;
+        z_old = i >> 12 ;
+        c_old = i >> 11 ;
+        alu_c_old = i >> 10 ;
         n_in = i >> 9 ;
         v_in = i >> 8 ;
         z_in = i >> 7 ;
@@ -43,12 +42,12 @@ TEST(test_pl6502, STATUS_ROM){
         alu_c_from_C = i >> 1 ;
         alu_c_set = i >> 0 ;
 
-        EXPECT_EQ(status_rom.N.get_value(), (nz_set ? n_in.get_value() : n_old)) ;
-        EXPECT_EQ(status_rom.V.get_value(), (v_set ? v_in.get_value() : v_old)) ;
+        EXPECT_EQ(status_rom.N.get_value(), (nz_set ? n_in.get_value() : n_old.get_value())) ;
+        EXPECT_EQ(status_rom.V.get_value(), (v_set ? v_in.get_value() : v_old.get_value())) ;
         EXPECT_EQ(status_rom.B.get_value(), b_in.get_value()) ;
-        EXPECT_EQ(status_rom.C.get_value(), (c_set ? c_in.get_value() : c_old)) ;
-        EXPECT_EQ(status_rom.Z.get_value(), (nz_set ? z_in.get_value() : z_old)) ;
-        EXPECT_EQ(status_rom.alu_c.get_value(), (alu_c_set ? (alu_c_from_C ? c_old : c_in.get_value()) : alu_c_old)) ;
+        EXPECT_EQ(status_rom.C.get_value(), (c_set ? c_in.get_value() : c_old.get_value())) ;
+        EXPECT_EQ(status_rom.Z.get_value(), (nz_set ? z_in.get_value() : z_old.get_value())) ;
+        EXPECT_EQ(status_rom.alu_c.get_value(), (alu_c_set ? (alu_c_from_C ? c_old.get_value() : c_in.get_value()) : alu_c_old.get_value())) ;
 
         // Reset the set signals or else they may still be on when we set in inputs 
         // in the next test...
@@ -57,20 +56,6 @@ TEST(test_pl6502, STATUS_ROM){
         c_set = 0 ;
         alu_c_set = 0 ;
     }
-
-
-    status_rom.N.disconnect(status_rom.n_old) ;
-    status_rom.V.disconnect(status_rom.v_old) ;
-    status_rom.Z.disconnect(status_rom.z_old) ;
-    status_rom.C.disconnect(status_rom.c_old) ;
-    status_rom.alu_c.disconnect(status_rom.alu_c_old) ;
-
-    output<1> n_old, v_old, z_old, c_old, alu_c_old ;
-    n_old.connect(status_rom.n_old) ;
-    v_old.connect(status_rom.v_old) ;
-    z_old.connect(status_rom.z_old) ;
-    c_old.connect(status_rom.c_old) ;
-    alu_c_old.connect(status_rom.alu_c_old) ;
 
     /*
         SST39SF020A: 
