d36029bfb3322774f0fe5cc3ffcb1451f7ddd938
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-11-15 08:24:37 -0500

    Update test_05_STATUS.cpp

--- pl6502/test_05_STATUS.cpp
+++ pl6502/test_05_STATUS.cpp
@@ -70,8 +70,8 @@ TEST(test_pl6502, STATUS){
       VCC    20E    1Y1    2A4    1Y2    2A3    1Y3    2A2    1Y4    2A1
       10E    1A1    2Y4    1A2    2Y3    1A3    2Y2    1A4    2Y1    GND
                                                  
-                    0out3  Bin    0out2  1in    Cout0  Vin    Zout1  Nin
-             0in    Bout4  0in    1out5  Cin    Vout6  Zin    Nout7  
+                    0out3  Bin    0out2  1in    Vout6  Cin    Nout7  Zin
+             0in    Bout4  0in    1out5  Vin    Cout0  Nin    Zout1  
     */
     for (int i = 0 ; i < 512 ; i++){
         uint8_t data = i >> 1 ;
