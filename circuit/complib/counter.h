16e6063b24fff1f8be28a7fd39e5655ae3590fac
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-11-26 11:24:16 -0500

    ok

--- circuit/complib/counter.h
+++ circuit/complib/counter.h
@@ -12,13 +12,7 @@ or DOWN). The direction of counting is determined by which count input is pulsed
 is high.
 - A clear (CLR) input has been provided that forces all outputs to the low level when a high level is applied. The
 clear function is independent of the count and LOAD inputs.
-
-SN74HC161
-- Negative LOAD
-- A low level at the clear (CLR) input sets all four of the flip-flop outputs low, regardless of the levels of the CLK, load (LOAD), or enable inputs.
-- Both ENP and ENT must be high to count
-*/
-    
+*/  
 template <uint32_t W> class counter_updown : public component {
     public:
         input<1> up, down, load, clear ;
@@ -62,6 +56,56 @@ template <uint32_t W> class counter_updown : public component {
             }
             _prev_down = down ;
             bo = (((! down)&&(_cnt == 0)) ? 0 : 1) ;
+
+            data_out = _cnt ;
+        } ;
+} ;
+
+
+/*
+SN74HC161
+- Negative LOAD
+- A low level at the clear (CLR) input sets all four of the flip-flop outputs low, regardless of the levels of the CLK, load (LOAD), or enable inputs.
+- Both ENP and ENT must be high to count
+- Changes at control inputs (ENP, ENT, or LOAD) that modify the operating mode have no effect on the contents of the counter until clocking occurs.
+*/  
+template <uint32_t W> class counter : public component {
+    public:
+        input<1> clk, load, clear, enable ;
+        input<W> data_in ;
+        output<1> rco ;
+        output<W> data_out ;
+    private:
+        uint32_t _cnt ;
+        uint8_t _prev_clk ;
+
+    public:
+        counter() : clk(this), load(this), clear(this), enable(this), data_in(this) {
+            _cnt = 0 ;
+            rco = 0 ;
+            _prev_clk = 1 ;
+        } ;
+
+        operator uint32_t(){
+            return _cnt ;
+        }
+
+        void always(){
+            if (clear){
+                _cnt = 0 ;
+            }
+            
+            if (! load){
+                _cnt = data_in ;
+            }
+
+            if (enable){
+                if ((clk)&&(! _prev_clk)){
+                    _cnt = (_cnt + 1) & ((1 << W) - 1) ;
+                    rco = ((_cnt == ((1 << W) - 1)) ? 1 : 0) ;
+                }
+                _prev_clk = clk ;
+            }
 
             data_out = _cnt ;
         } ;
