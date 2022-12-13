1d444b6e616c7a2c69919201f3fcd4700a91a5d5
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2022-12-13 17:47:46 -0500

    ok

--- simulator/src/programs.h
+++ simulator/src/programs.h
@@ -1,3 +1,5 @@
 PROG progTestSuite("TestSuite", "programs/testsuite/6502_functional_test.bin", 0, 0, 0, 0x3680) ;
 
+PROG progHello("Hello", "programs/hello/hello.bin", 0, 0, 0, 0x20) ;
+
 PROG progStar("Star", "programs/star/star.bin", 0x0100, 0, 0, 0x011A) ;
